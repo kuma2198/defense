f11459feeeee517097f7007da4e82db21091f2fd
kuma2198 <70785322+kuma2198@users.noreply.github.com>
2022-06-08 00:02:06 +0900

    Update tokyopopu.py

--- src/tokyopopu.py
+++ src/tokyopopu.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 import sys,os
 
 
-if os.path.exists(tokyo_population.csv’):
-  df=pd.read_csv(tokyo_population.csv’,encoding='shift-jis')
+if os.path.exists('tokyo_population.csv’):
+  df=pd.read_csv('tokyo_population.csv’,encoding='shift-jis')
 else:
-print(“You need to download csv from the following site:”)
-print(https://www.city.inagi.tokyo.jp/shisei/gyosei/opendata/opendata_catalogpage/zinkou.files/010.csv”) (accessed on 7 June 2022)
+print("You need to download csv from the following site:")
+print("https://www.city.inagi.tokyo.jp/shisei/gyosei/opendata/opendata_catalogpage/zinkou.files/010.csv”) (accessed on 7 June 2022)
 
 
 def main():
